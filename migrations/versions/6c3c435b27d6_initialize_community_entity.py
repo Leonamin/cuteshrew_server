17cc4019cf4369562a4c3c0c7e6f753019a8903f
Leonamin <yism810@gmail.com>
2022-07-26 22:20:13 +0900

    modify initial revision

--- migrations/versions/6c3c435b27d6_initialize_community_entity.py
+++ migrations/versions/6c3c435b27d6_initialize_community_entity.py
@@ -5,10 +5,12 @@ Revises:
 Create Date: 2022-07-24 21:18:30.015087
 
 """
+import time
 from alembic import op
 import sqlalchemy as sa
 
 from app.dependency import Authority
+from app.hashing import Hash
 
 
 # revision identifiers, used by Alembic.
@@ -19,15 +21,12 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.create_table('users',
-                    sa.Column('id', sa.Integer(), nullable=False,
-                              primary_key=True),
-                    sa.Column('nickname', sa.String(), nullable=False),
-                    sa.Column('email', sa.String(), nullable=False),
-                    sa.Column('password', sa.String(), nullable=False),
-                    sa.Column('authority', sa.Enum(Authority), nullable=False),
-                    sa.Column('created_at', sa.BigInteger(), nullable=False))
+    op.execute('''
+                INSERT INTO users (id, nickname, email, password, authority, created_at)
+                VALUES (1, 'admin', 'admin', '{0}', 'GOD', {1}); '''.format(Hash.bcrypt('admin'), int(time.time())))
 
 
 def downgrade() -> None:
-    op.drop_table('users')
+    op.execute('''
+                DELETE FROM users WHERE ("id" = "1")
+    ''')
