6348131224319e71b7ee957f10ed82a25357253b
Leonamin <yism810@gmail.com>
2022-11-26 21:47:01 +0900

    리팩토링 - 게시글 API에 새로운 스키마 적용 및 비밀번호 관련 예외처리

--- app/repository/postingRepo.py
+++ app/repository/postingRepo.py
@@ -3,25 +3,8 @@ from fastapi import HTTPException, Response, status
 from app.dependency import Authority
 from app.hashing import Hash
 
-from app.oauth2 import get_current_user
-from .. import models, schemas
+from .. import models, new_schemas
 import time
-
-# 속한 커뮤니티의 게시글 로딩
-# /community/general/12313
-
-
-def get_all(name: str, db: Session):
-    community = db.query(models.Community).filter(
-        models.Community.name == name)
-    if not community.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Community with the name {name} not found")
-
-    postings = db.query(models.Posting).filter(
-        models.Posting.community_id == community.first().id).all()
-
-    return postings
 
 
 def get_post(name: str, post_id: int, password: str, db: Session):
@@ -45,12 +28,28 @@ def get_post(name: str, post_id: int, password: str, db: Session):
         if not Hash.verify(posting.password, password):
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                                 detail=f"Invalid password")
-    return posting
+            
+    return new_schemas.ResponsePosting(
+        id=posting.id,
+        title=posting.title,
+        body=posting.body,
+        is_locked=posting.is_locked,
+        published_at=posting.published_at,
+        updated_at=posting.updated_at,
+        creator=new_schemas.UserBase(
+            nickname=posting.creator.nickname, 
+            email=posting.creator.email),
+        own_community=new_schemas.CommunityBase(
+            id=posting.own_community.id,
+            name=posting.own_community.name,
+            showname=posting.own_community.showname,
+            ),
+        )
 
 # FIXME password가 공백이여도 암호가 생성된다 nullable로 만들어야해!
 
 
-def create_post(name: str, reqeust: schemas.PostingCreate, db: Session, request_user: schemas.User):
+def create_post(name: str, reqeust: new_schemas.PostingCreate, db: Session, request_user: new_schemas.UserBase):
     community = db.query(models.Community).filter(
         models.Community.name == name)
     if not community.first():
@@ -64,6 +63,16 @@ def create_post(name: str, reqeust: schemas.PostingCreate, db: Session, request_
     if not (user.first().authority.value >= community.first().authority.value):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail=f"User has low authoriy {user.first().authority}")
+    
+    # 잠긴 게시물이 아니면 비밀번호 없음
+    if (reqeust.is_locked == False) :
+        password = None
+    else:
+        # 잠긴 게시물로 설정해도 비밀번호가 없거나 길이가 0이면 생성금지
+        if (reqeust.password is None) or (len(reqeust.password) <= 0):
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail=f"password can't be null or empty")
+        password = Hash.bcrypt(reqeust.password)
 
     new_post = models.Posting(
         title=reqeust.title,
@@ -71,7 +80,7 @@ def create_post(name: str, reqeust: schemas.PostingCreate, db: Session, request_
         published_at=int(time.time()),
         updated_at=int(time.time()),
         is_locked=reqeust.is_locked,
-        password=Hash.bcrypt(reqeust.password),
+        password=password,
         community_id=community.first().id,
         user_id=user.first().id,
     )
@@ -81,7 +90,7 @@ def create_post(name: str, reqeust: schemas.PostingCreate, db: Session, request_
     return "Post is created!"
 
 
-def update_post(name: str, post_id: int, reqeust: schemas.PostingCreate, db: Session, request_user: schemas.User):
+def update_post(name: str, post_id: int, reqeust: new_schemas.PostingCreate, db: Session, request_user: new_schemas.UserBase):
     community = db.query(models.Community).filter(
         models.Community.name == name)
     if not community.first():
@@ -104,13 +113,22 @@ def update_post(name: str, post_id: int, reqeust: schemas.PostingCreate, db: Ses
                                 detail=f"User has low authoriy {user.first().authority}")
 
     posting.update(reqeust.dict())
-    posting.update({'password': Hash.bcrypt(reqeust.password)})
+    # 잠긴 게시물이 아니면 비밀번호 없음
+    if (reqeust.is_locked == False) :
+        password = None
+    else:
+        # 잠긴 게시물로 설정해도 비밀번호가 없거나 길이가 0이면 생성금지
+        if (reqeust.password is None) or (len(reqeust.password) <= 0):
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail=f"password can't be null or empty")
+        password = Hash.bcrypt(reqeust.password)
+    posting.update({'password': password})
     posting.update({'updated_at': int(time.time())})
     db.commit()
     return 'updated'
 
 
-def delete_post(name: str, post_id: int, db: Session, request_user: schemas.User):
+def delete_post(name: str, post_id: int, db: Session, request_user: new_schemas.UserBase):
     community = db.query(models.Community).filter(
         models.Community.name == name)
     if not community.first():
--- app/routers/posting.py
+++ app/routers/posting.py
@@ -2,7 +2,7 @@ from typing import List, Optional
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
-from .. import schemas, database
+from .. import new_schemas, database
 from ..repository import postingRepo
 from ..oauth2 import get_current_user
 
@@ -11,29 +11,21 @@ router = APIRouter(
     tags=['posting']
 )
 
-
-@router.get("")
-def get_all(name: str, db: Session = Depends(database.get_db)):
-    return postingRepo.get_all(name, db)
-
-
-@router.get("/{id}", response_model=schemas.PostingResponse)
+@router.get("/{id}", response_model=new_schemas.ResponsePosting, response_model_exclude_none= True)
 def get_post(name: str, id: int, password: Optional[str] = None, db: Session = Depends(database.get_db)):
     return postingRepo.get_post(name, id, password, db)
 
 
 @router.post("", status_code=status.HTTP_201_CREATED)
-def create_post(name: str, request: schemas.PostingCreate, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(get_current_user)):
+def create_post(name: str, request: new_schemas.PostingCreate, db: Session = Depends(database.get_db), current_user: new_schemas.UserBase = Depends(get_current_user)):
     return postingRepo.create_post(name, request, db, current_user)
-
-# update 할 때 .dict()로 형변환...
 
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update_post(name: str, id: int, request: schemas.PostingCreate, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(get_current_user)):
+def update_post(name: str, id: int, request: new_schemas.PostingCreate, db: Session = Depends(database.get_db), current_user: new_schemas.UserBase = Depends(get_current_user)):
     return postingRepo.update_post(name, id, request, db, current_user)
 
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(name: str, id: int, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(get_current_user)):
+def delete_post(name: str, id: int, db: Session = Depends(database.get_db), current_user: new_schemas.UserBase = Depends(get_current_user)):
     return postingRepo.delete_post(name, id, db, current_user)
