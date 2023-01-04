8379f7d305f042397298043bd0f4d307e920253f
Leonamin <51023129+Leonamin@users.noreply.github.com>
2023-01-04 14:05:00 +0900

    핫픽스

--- app/repository/searchRepo.py
+++ app/repository/searchRepo.py
@@ -6,7 +6,7 @@ from ..models import models
 
 from ..schemas import new_schemas
 
-
+# FIXME 생각해보니 Response 모델 하나로 돌려쓰는건 바보같은 짓이고 세부적으로 나눠야할 필요가 있다.
 # user_id와 user_name 둘중 하나는 있어야함
 # start_post_id는 원래 해당 포스팅 부터 시작하게하려고 했으나 기술적인 이유로 포기 그냥 필터링 된 개수에서 n번부터 시작함
 # 2022-11-17 해결 방법은 매우 간단하게도 .where(조건)을 사용하면 조건을 만족하는 것 이후부터 찾을 수 있다!
@@ -65,6 +65,11 @@ def search_posts_by_user(user_id: int, user_name: str, start_id: int, load_page_
         posting_response = new_schemas.ResponsePostingPreview.from_orm(posting)
         posting_response.comment_count = comment_count
         posting_response.user_id = None
+        
+        if (posting_response.posting.own_community is not None):
+                posting_response.own_community.authority = None
+                posting_response.own_community.created_at = None
+                posting_response.own_community.published_at = None
         posting_response.own_community.authority = None
         posting_response.own_community.created_at = None
         posting_response.own_community.published_at = None
@@ -127,10 +132,13 @@ def search_comments_by_user(user_id: int, user_name: str, start_id: int, load_pa
     
     for comment in comments_db:
         comment_response = new_schemas.ResponseComment.from_orm(comment)
-        comment_response.posting.own_community.authority = None
-        comment_response.posting.own_community.created_at = None
-        comment_response.posting.own_community.published_at = None
-        comment_response.posting.user_id = None
+        # 삭제 되었을 경우 posting이 None이기 때문에 하위 필드 접근이 안된다.
+        if (comment_response.posting is not None):
+            if (comment_response.posting.own_community is not None):
+                comment_response.posting.own_community.authority = None
+                comment_response.posting.own_community.created_at = None
+                comment_response.posting.own_community.published_at = None
+            comment_response.posting.user_id = None
         comments.append(comment_response)
 
     return new_schemas.ResponseCommentList(comment_count=comment_counts, comments=comments)
