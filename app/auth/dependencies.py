06e3aed4175c9d8c0ea9ddc01a01daa5a7239a64
Leonamin <51023129+Leonamin@users.noreply.github.com>
2023-01-06 14:54:26 +0900

    추가 - 토큰 생성 의존성 추가

--- app/auth/dependencies.py
+++ app/auth/dependencies.py
@@ -1,11 +1,28 @@
+from datetime import datetime, timedelta
+from typing import Optional
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 
-from app.auth.constants import ALGORITHM, SECRET_KEY
+from app.auth.constants import ALGORITHM
 from app.auth.exceptions import InvalidCredentials
 from app.auth.schemas import AuthToken
 from app.auth.utils import getCurrentUnixTimeStamp
+from app.dependency import get_secret_key
+
+# data = {"sub" : user.email}
+def create_access_token(data: dict, 
+    expires_delta: Optional[timedelta],
+    secret_ket: str = Depends(get_secret_key)
+):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, secret_ket, algorithm=ALGORITHM)
+    return encoded_jwt, expire
 
 def valid_token(token: AuthToken) -> AuthToken:
     if token.token_type != 'bearer':
@@ -17,10 +34,11 @@ def valid_token(token: AuthToken) -> AuthToken:
     return token
 
 def parse_jwt_data(
-    token: str = Depends(OAuth2PasswordBearer(tokenUrl="/auth/token"))
+    token: str = Depends(OAuth2PasswordBearer(tokenUrl="/auth/token")),
+    secret_key: str = Depends(get_secret_key)
 ) -> dict:
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        payload = jwt.decode(token, secret_key, algorithms=[ALGORITHM])
     except JWTError:
         raise InvalidCredentials()
         # raise "InvalidCredentials"
