612ace67eb0b00209845ec3fa12f3da5b558bf8e
Leonamin <51023129+Leonamin@users.noreply.github.com>
2023-01-06 21:21:15 +0900

    추가 - 유저 정보 스키마로 불러오게 수정

--- app/user/router.py
+++ app/user/router.py
@@ -2,7 +2,7 @@ from typing import Mapping
 from fastapi import APIRouter, Depends, status
 
 from .dependency import valid_user_name, valid_user_email
-from .schemas import RequestUserCreate
+from .schemas import RequestUserCreate, ResponseUserDetail
 from .exceptions import UserNotFoundException
 
 
@@ -23,6 +23,6 @@ def create_user_for_admin():
     pass
 
 
-@router.get('/search')
+@router.get('/search', response_model=ResponseUserDetail)
 async def get_user_by_name(user: Mapping = Depends(valid_user_name)):
     return user
--- app/user/schemas.py
+++ app/user/schemas.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import BaseModel, SecretStr
 
 
@@ -5,3 +6,32 @@ class RequestUserCreate(BaseModel):
     nickname: str
     email: str
     password: SecretStr
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "nickname": "best_shrew",
+                "email": "best_shrew@cuteshrew.xyz",
+                "password": "password_what_you_want",
+            }
+        }
+
+
+class ResponseUserDetail(BaseModel):
+    nickname: str
+    email: str
+    posting_count: Optional[int]
+    comment_count: Optional[int]
+    introduction: Optional[str]
+
+    class Config():
+        orm_mode = True
+        schema_extra = {
+            "example": {
+                "nickname": "best_shrew",
+                "email": "best_shrew@cuteshrew.xyz",
+                "posting_count": 31,
+                "comment_count": 5,
+                "introduction": "let me tell you something... ACTUALLY I'M HAMSTER"
+            }
+        }
