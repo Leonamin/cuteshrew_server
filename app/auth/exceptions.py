2ea959a2578c9e6f51aca15c923eb0bec48ff662
Leonamin <51023129+Leonamin@users.noreply.github.com>
2023-01-06 22:30:39 +0900

    추가 - 예외 추가

--- app/auth/exceptions.py
+++ app/auth/exceptions.py
@@ -1,5 +1,19 @@
 from fastapi import HTTPException, status
 
-def InvalidCredentials():
-    return HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                detail=f"Invaild Credentials")
+
+# def InvalidCredentials():
+#     return HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+#                          detail=f"Invaild Credentials")
+
+
+class InvalidCredentials(HTTPException):
+    def __init(self):
+        self.status_code = status.HTTP_401_UNAUTHORIZED
+        self.detail = "Could not validate credentials",
+        self.headers = {"WWW-Authenticate": "Bearer"},
+
+
+class IncorrectPassword(HTTPException):
+    def __init(self):
+        self.status_code = status.HTTP_404_NOT_FOUND
+        self.detail = "Incorrect Password",
