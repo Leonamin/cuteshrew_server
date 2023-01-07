561d9e95907d31b8873ba39ca6d37a10c13ad088
Leonamin <51023129+Leonamin@users.noreply.github.com>
2023-01-07 17:49:33 +0900

    추가 - 범용 예외 추가

--- app/exceptions.py
+++ app/exceptions.py
@@ -7,3 +7,21 @@ def DatabaseError(detail: str):
     return HTTPException(
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
         detail=detail)
+
+
+def HashValueError():
+    return HTTPException(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        detail="Hash method failed because of value error")
+
+
+def HashTypeError():
+    return HTTPException(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        detail="Hash method failed because of type error")
+
+
+def UnknownError(detail: str):
+    return HTTPException(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        detail=detail)
