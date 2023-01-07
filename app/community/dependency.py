08cb13a73aa907ca6f57880c2bcafc82547458bf
Leonamin <51023129+Leonamin@users.noreply.github.com>
2023-01-07 16:28:07 +0900

    추가 - 커뮤니티 유효 이름, 유효 가져오는 개수 추가

--- app/community/dependency.py
+++ app/community/dependency.py
@@ -6,12 +6,23 @@ from fastapi import Depends
 from app.dependency import Authority
 
 from .service import get_community_by_name
-from .exceptions import CommunityNotFound
+from .exceptions import CommunityNotFound, InvalidLoadCountException, InvalidCommunityName
 
 
 async def valid_community_name(community_name: str) -> Mapping:
+    if community_name == 'info' or \
+            community_name == 'all' or \
+            community_name == 'page':
+        raise InvalidCommunityName()
+
     community = await get_community_by_name(community_name=community_name)
 
     if not community:
         raise CommunityNotFound()
     return community
+
+
+def valid_load_cound(load_count: int) -> int:
+    if (load_count > 100 or load_count <= 0):
+        raise InvalidLoadCountException()
+    return load_count
