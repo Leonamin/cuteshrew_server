f394cf0267e8f6b1aea1bdd5562074c3dec0cf47
Leonamin <51023129+Leonamin@users.noreply.github.com>
2023-01-07 17:51:32 +0900

    추가 - 게시글 예외 추가

---
+++ app/posting/exceptions.py
@@ -0,0 +1,26 @@
+from fastapi import HTTPException, status
+
+
+def PostingNotFound():
+    return HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                         detail="Posting not found")
+
+
+def UnauthorizedException():
+    return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                         detail="Your don't have permission")
+
+
+def InvalidLoadCountException():
+    return HTTPException(status_code=status.HTTP_431_REQUEST_HEADER_FIELDS_TOO_LARGE,
+                         detail="Load count must be less than 100 or over than 0")
+
+
+def NeedPasswordException():
+    return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                         detail="You must send password")
+
+
+def InvalidPasswordException():
+    return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                         detail="Invalid password")
