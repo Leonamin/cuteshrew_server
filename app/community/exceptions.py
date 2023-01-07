3bf1ad729f059ad0f7f0985c5253229538c51fc5
Leonamin <51023129+Leonamin@users.noreply.github.com>
2023-01-07 16:27:47 +0900

    추가 - 커뮤니티 예외 추가

--- app/community/exceptions.py
+++ app/community/exceptions.py
@@ -9,3 +9,13 @@ def CommunityNotFound():
 def UnauthorizedException():
     return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                          detail="Your don't have permission")
+
+
+def InvalidLoadCountException():
+    return HTTPException(status_code=status.HTTP_431_REQUEST_HEADER_FIELDS_TOO_LARGE,
+                         detail="Load count must be less than 100 or over than 0")
+
+
+def InvalidCommunityName():
+    return HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                         detail="/info /page /all is not valid name for community")
