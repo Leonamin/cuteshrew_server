478d7b22df2523cb83eed169173795aa3234a602
Leonamin <51023129+Leonamin@users.noreply.github.com>
2023-01-07 15:54:18 +0900

    추가 - database 예외 추가

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,1 +1,9 @@
 # Global Exception
+
+from fastapi import HTTPException, status
+
+
+def DatabaseError(detail: str):
+    return HTTPException(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        detail=detail)
