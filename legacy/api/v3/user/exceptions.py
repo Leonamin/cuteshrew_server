b5105c792cdae91863240627ffcacc7c694497e7
Leonamin <yism810@gmail.com>
2025-06-18 17:57:52 +0900

    # DELETE - 레거시 삭제

--- legacy/api/v3/user/exceptions.py
+++
@@ -1,6 +0,0 @@
-from fastapi import HTTPException, status
-
-
-def UserNotFoundException():
-    return HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                         detail="User not found")
--- legacy/db/initialize.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Engine
-from app.data.models import comment, community, post, user
-
-def create_db_metadata(engine: Engine):
-    community.Base.metadata.create_all(bind=engine)
-    post.Base.metadata.create_all(bind=engine)
-    comment.Base.metadata.create_all(bind=engine)
-    user.Base.metadata.create_all(bind=engine)
